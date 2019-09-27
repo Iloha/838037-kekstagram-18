285f533afdd081d15c9345c8b2e62230ef56fff1
Ilona Khafizova <ilona.khafizova@workato.com>
2019-09-27 14:00:17 -0700

    added handlers to open and close editForm

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var IMAGES_NUMBER = 25;
 var AVATAR_MAX = 6;
 var LIKES_MIN = 15;
 var LIKES_MAX = 200;
+var ESC_CODE = 27;
 
 var imagesTemplate = document.querySelector('#picture')
   .content
@@ -88,3 +89,43 @@ var renderImagesList = function (array) {
 var imagesArray = generateArray(IMAGES_NUMBER);
 var fragment = renderImagesList(imagesArray);
 imagesList.appendChild(fragment);
+
+
+var uploadFile = document.querySelector('#upload-file');
+var editForm = document.querySelector('.img-upload__overlay');
+var closeEditForm = editForm.querySelector('#upload-cancel')
+
+var showEditForm = function () {
+  editForm.classList.remove('hidden');
+  closeEditForm.addEventListener('click', onCloseForm);
+  document.addEventListener('keydown', onPressEscClose);
+};
+
+var closeForm = function () {
+  editForm.classList.add('hidden');
+  document.removeEventListener('change', onUploadFileChange);
+  document.removeEventListener('click', onCloseForm);
+  document.removeEventListener('keydown', onPressEscClose);
+}
+
+var onUploadFileChange = function () {
+  showEditForm();
+}
+
+var onCloseForm = function () {
+  closeForm();
+
+  //remove all handlers
+}
+
+var onPressEscClose = function(evt) {
+  if (evt.keyCode === ESC_CODE) {
+    closeForm();
+  }
+}
+
+uploadFile.addEventListener('change', onUploadFileChange);
+
+
+// На клик по крестику вешаю обработчик на закрывтие формы, и esc
+//который вызывает функцию closeForm, в которой также сбрасывается значение поля выбора файла #upload-file
