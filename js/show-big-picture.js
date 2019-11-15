481140a03428ad262640b48a6ff5ea48e7a2fce0
Ilona Khafizova <ilona.khafizova@workato.com>
2019-11-15 12:43:11 -0800

    added button to load comments

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -11,32 +11,55 @@
   var commentsLoader = bigPicture.querySelector('.comments-loader');
   var addCommentField = bigPicture.querySelector('.social__footer-text');
   var bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+  var showenCommentsAmount;
+  var currentPicture;
 
   window.showBigPicture = function (picture) {
+    showenCommentsAmount = 0;
     bigPicture.classList.remove('hidden');
-    fillBigPicture(picture);
+    currentPicture = picture;
+    fillBigPicture(currentPicture);
   };
 
   var fillBigPicture = function (picture) {
     bigPictureImg.src = picture.url;
     likesCount.textContent = picture.likes;
-    commentsCount.textContent = picture.comments.length;
+    showNextComments();
+    // commentsCount.textContent = picture.comments.length;
     socialCaption.textContent = picture.description;
     renderCommentsTemplate(picture.comments);
   };
 
   var renderCommentsTemplate = function (commentsList) {
     var template = '';
-    for (var i = 0; i < window.data.COMMENTS_NUMBER; i++) {
+    for (var i = 0; i < showenCommentsAmount; i++) {
       template += '<li class="social__comment"><img class="social__picture" src="' + commentsList[i].avatar + '" alt="' + commentsList[i].name + '" width="35" height="35"><p class="social__text">' + commentsList[i].message + '</p></li>';
     }
     commentsWrapper.innerHTML = template;
   };
 
-  var hideComments = function () {
-    commentsCountWrapper.classList.add('visually-hidden');
+  var hideLoadMoreComments = function () {
     commentsLoader.classList.add('visually-hidden');
   };
+
+  var showLoadMoreComments = function () {
+    commentsLoader.classList.remove('visually-hidden');
+  };
+
+  var showNextComments = function () {
+    showLoadMoreComments();
+    showenCommentsAmount += window.data.COMMENTS_NUMBER;
+    if (showenCommentsAmount >= currentPicture.comments.length) {
+      showenCommentsAmount = currentPicture.comments.length;
+      hideLoadMoreComments();
+    }
+    renderCommentsTemplate(currentPicture.comments);
+    commentsCountWrapper.innerHTML = showenCommentsAmount + ' из <span class="comments-count">' + currentPicture.comments.length + '</span> комментариев';
+  };
+  // var hideComments = function () {
+  //   commentsCountWrapper.classList.add('visually-hidden');
+  //
+  // };
 
   var closeBigPicture = function () {
     bigPicture.classList.add('hidden');
@@ -53,8 +76,19 @@
     }
   };
 
-  hideComments();
+
+
+  var onClickShowComments = function () {
+    showNextComments();
+  };
+  // hideComments();
   bigPictureCancel.addEventListener('click', onCloseBigPicture);
   document.addEventListener('keydown', onPressEscBigPicture);
+  commentsLoader.addEventListener('click', onClickShowComments);
 
 })();
+
+// 1. Показывать 5 комментов
+// 2. При нажатии на кнопку показывать следующие 5
+// 3. При нажатии на кнопку повесить хэндлер с вызовом фунции showNext5
+// 4. showNext5 функция, которая идет по массиву и показывает следующие 5
